a2a892cc80bf36842a1d4fd5f53fa43bbd9c42f6
Khalil Gorsan Mestiri <khalilgorsan@gmail.com>
2020-05-17 02:30:56 +0100

    Refactor: Separate Hill climbing and its variants

--- trainer.py
+++ trainer.py
@@ -1,15 +1,28 @@
 from policy import Policy
-from policy_based_agents import hill_climbing
+from policy_based_agents import hill_climbing, steepest_ascent_hill_climbing
 
 
 class Trainer:
-    def __init__(self, policy):
-        score = hill_climbing(policy=policy)
+    def __init__(self, population_size=1, variant="hill_climbing"):
+        self.population_size = population_size
+        self.policies = [Policy() for _ in range(population_size)]
+        self.variant = variant
+
+    def train(self):
+        if self.variant == "hill_climbing":
+            assert (
+                self.population_size == 1
+            ), "Please make sure that we use one policy in hill climbing"
+            self.score = hill_climbing(self.policies[0])
+        elif self.variant == "steepest_ascent_hill_climbing":
+            self.score = steepest_ascent_hill_climbing(
+                policies=self.policies, population_size=self.population_size
+            )
 
 
 def main():
-    policy = Policy()
-    trainer = Trainer(policy)
+    trainer = Trainer()
+    trainer.train()
 
 
 if __name__ == "__main__":
